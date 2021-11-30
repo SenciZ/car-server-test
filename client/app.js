00d729c8305ea68640320620fe237e30bfe056b2
Senad Zenkovic <zenkovic88@gmail.com>
2021-11-29 18:23:12 -0700

    create application structure

--- client/app.js
+++ client/app.js
@@ -1,3 +1,15 @@
-// alert('connected')
-
 const newCarBtn = document.getElementById('generateCar')
+newCarBtn.addEventListener('click', ()=>{
+    axios
+    .get('http://localhost:5000/cars')
+    .then(response , ()=>{
+        let randomCar = Math.floor(Math.random() * response.data.length)
+        renderNewCar(response.data[randomCar])
+    })
+});
+
+
+const renderNewCar = (dataArr) =>{
+    const mainImage = document.getElementById('mainImage')
+    mainImage.style.background = `url('${dataArr.location}')`
+}
---
+++ server/index.js
@@ -0,0 +1,9 @@
+const cars = [
+    {name: "Yellow Mercedes Benz", location: 'https://images.pexels.com/photos/2365572/pexels-photo-2365572.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+
+    {name: "Gray Lamborghini Diablo", location: 'https://images.pexels.com/photos/3752169/pexels-photo-3752169.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'},
+
+    {name: "Yellow Ferrari 458 California", location: 'https://images.pexels.com/photos/3954441/pexels-photo-3954441.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+
+    {name: "Cooper Chevrolet Corvete Z06", location: 'https://images.pexels.com/photos/3972755/pexels-photo-3972755.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'},
+]
