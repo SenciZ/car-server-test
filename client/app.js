d5fddd9bf0b3f57dfdf70906b722f39d67a58a7b
Senad Zenkovic <zenkovic88@gmail.com>
2021-11-29 19:06:10 -0700

    complete full stack app

--- client/app.js
+++ client/app.js
@@ -1,9 +1,11 @@
 const newCarBtn = document.getElementById('generateCar')
+
 newCarBtn.addEventListener('click', ()=>{
     axios
-    .get('http://localhost:5000/cars')
-    .then(response , ()=>{
+    .get('http://localhost:5000/api/cars')
+    .then(response => {
         let randomCar = Math.floor(Math.random() * response.data.length)
+        console.log(response.data)
         renderNewCar(response.data[randomCar])
     })
 });
@@ -11,5 +13,10 @@ newCarBtn.addEventListener('click', ()=>{
 
 const renderNewCar = (dataArr) =>{
     const mainImage = document.getElementById('mainImage')
-    mainImage.style.background = `url('${dataArr.location}')`
+    const carDetails = document.getElementById("carDetails")
+    carDetails.textContent = dataArr.name;
+    mainImage.style.background = `url('${dataArr.location}')`;
+    mainImage.style.backgroundSize='cover'
+    mainImage.style.backgroundPosition="center"
 }
+
--- server/index.js
+++ server/index.js
@@ -1,9 +1,37 @@
-const cars = [
-    {name: "Yellow Mercedes Benz", location: 'https://images.pexels.com/photos/2365572/pexels-photo-2365572.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+const express = require('express')
+const cors = require('cors')
 
-    {name: "Gray Lamborghini Diablo", location: 'https://images.pexels.com/photos/3752169/pexels-photo-3752169.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'},
+const app = express();
+app.use(cors())
+app.use(express.json())
 
-    {name: "Yellow Ferrari 458 California", location: 'https://images.pexels.com/photos/3954441/pexels-photo-3954441.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
 
-    {name: "Cooper Chevrolet Corvete Z06", location: 'https://images.pexels.com/photos/3972755/pexels-photo-3972755.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'},
-]
+// const cars = [
+//     {name: "Yellow Mercedes Benz", location: 'https://images.pexels.com/photos/2365572/pexels-photo-2365572.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+
+//     {name: "Gray Lamborghini Diablo", location: 'https://images.pexels.com/photos/3752169/pexels-photo-3752169.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'},
+
+//     {name: "Yellow Ferrari 458 California", location: 'https://images.pexels.com/photos/3954441/pexels-photo-3954441.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+
+//     {name: "Cooper Chevrolet Corvete Z06", location: 'https://images.pexels.com/photos/3972755/pexels-photo-3972755.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'},
+// ]
+
+app.get('/api/cars', (req, res)=>{
+
+    const cars = [
+        {name: "Yellow Mercedes Benz", location: 'https://images.pexels.com/photos/2365572/pexels-photo-2365572.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+    
+        {name: "Gray Lamborghini Diablo", location: 'https://images.pexels.com/photos/3752169/pexels-photo-3752169.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'},
+    
+        {name: "Yellow Ferrari 458 California", location: 'https://images.pexels.com/photos/3954441/pexels-photo-3954441.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+    
+        {name: "Cooper Chevrolet Corvete Z06", location: 'https://images.pexels.com/photos/3972755/pexels-photo-3972755.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'},
+    ];
+
+
+    res.status(200).send(cars)
+})
+
+app.listen(5000, ()=>{
+    console.log("Server is running on port 5000");
+})
